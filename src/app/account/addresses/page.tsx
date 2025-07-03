b1fc7175ba3f68c4065a5378e682f15762375ee5
Juanjo <calvoledesma91@gmail.com>
2025-07-03 08:35:33 -0300

    Tabs added to direction in account page

--- src/app/account/addresses/page.tsx
+++ src/app/account/addresses/page.tsx
@@ -5,23 +5,34 @@ import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import { AddressList } from "@/components/account/address-list";
 import { Footer } from "@/components/common/footer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function AddressesPage() {
   const router = useRouter();
 
   return (
     <div className="container mx-auto px-6 pt-[55px] min-h-screen flex flex-col justify-between">
-      <div className="space-y-4 flex-grow">
-        <div className="flex justify-between">
-          <Button onClick={() => router.back()} variant="ghost" size="icon">
-            <ChevronLeftIcon className="w-6 h-6" />
-          </Button>
-          <h1 className="body-01-medium text-text-primary-default uppercase">
-            Direcciones
-          </h1>
-        </div>
-        <AddressList />
+      <div className="flex justify-between">
+        <Button onClick={() => router.back()} variant="ghost" size="icon">
+          <ChevronLeftIcon className="w-6 h-6" />
+        </Button>
+        <h1 className="body-01-medium text-text-primary-default uppercase">
+          Direcciones
+        </h1>
       </div>
+      <Tabs defaultValue="principal" className="flex-grow space-y-4">
+        <TabsList>
+          <TabsTrigger
+            value="principal"
+            className="flex-1 text-center uppercase"
+          >
+            Principal
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="principal" className="space-y-4">
+          <AddressList />
+        </TabsContent>
+      </Tabs>
       <Footer />
     </div>
   );
