1bd96d5e5aeb9d32b6c513f927c409785ddcd667
Juanjo <calvoledesma91@gmail.com>
2025-07-06 18:57:27 -0300

    Animation added to 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,32 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
+import { motion } from "framer-motion";
 import { Footer } from "@/components/common/footer";
 
 export default function NotFound() {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <div className="min-h-screen flex flex-col justify-between bg-background-primary-default px-6">
-      <div className="flex flex-col items-center justify-center flex-grow space-y-4 text-center">
+      <motion.div
+        className="flex flex-col items-center justify-center flex-grow space-y-4 text-center"
+        initial={{ opacity: 0, filter: "blur(8px)" }}
+        animate={{
+          opacity: imageLoaded ? 1 : 0,
+          filter: imageLoaded ? "blur(0px)" : "blur(8px)",
+        }}
+        transition={{ duration: 0.5 }}
+      >
+        {" "}
         <Image
           src="/illustrations/404-image.svg"
           alt="404"
           width={240}
           height={240}
           className="mb-4"
+          onLoad={() => setImageLoaded(true)}
         />
         <h1 className="heading-06-medium text-text-primary-default">
           Página no encontrada
@@ -25,7 +40,7 @@ export default function NotFound() {
         >
           Volver al inicio
         </Link>
-      </div>
+      </motion.div>{" "}
       <Footer />
     </div>
   );
