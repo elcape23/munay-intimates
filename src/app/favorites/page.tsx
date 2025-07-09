4498c48f911889568a503de8c23cdc8e3b44f260
Juanjo <calvoledesma91@gmail.com>
2025-07-09 11:10:38 -0300

    Empty favorites page title added

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -7,13 +7,16 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import { motion } from "framer-motion";
-
+import { useRouter } from "next/navigation";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { Button } from "@/components/ui/button";
 import { Footer } from "@/components/common/footer";
 import { ProductGrid } from "@/components/collections/product-grid";
 
 export default function FavoritesPage() {
   const { favoriteProducts, isLoading } = useFavoritesStore();
   const [imageLoaded, setImageLoaded] = useState(false);
+  const router = useRouter();
 
   if (isLoading) {
     return (
@@ -28,6 +31,14 @@ export default function FavoritesPage() {
   if (!favoriteProducts || favoriteProducts.length === 0) {
     return (
       <main className="container mx-auto px-6 pt-[55px] min-h-screen flex flex-col justify-between">
+        <div className="flex flex-row items-center justify-between gap-2 mb-4">
+          <Button onClick={() => router.back()} variant="ghost" size="icon">
+            <ChevronLeftIcon className="h-6 w-6 text-icon-primary-default" />
+          </Button>
+          <h1 className="body-01-medium uppercase tracking-tight text-text-primary-default">
+            FAVORITOS
+          </h1>
+        </div>
         <motion.div
           className="flex flex-col items-center justify-center flex-grow text-gray-500"
           initial={{ opacity: 0, filter: "blur(8px)" }}
@@ -37,7 +48,6 @@ export default function FavoritesPage() {
           }}
           transition={{ duration: 0.5 }}
         >
-          {" "}
           <Image
             src="/illustrations/fav-empty.svg"
             alt="Favoritos vacío"
@@ -55,7 +65,7 @@ export default function FavoritesPage() {
           >
             Explorar productos
           </Link>
-        </motion.div>{" "}
+        </motion.div>
         <Footer />
       </main>
     );
