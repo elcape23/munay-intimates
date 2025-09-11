1d6817a1b3ceef180e7385af805016faaecd8c77
Juanjo <calvoledesma91@gmail.com>
2025-09-11 11:18:17 -0300

    Styling delivery schedule modal

--- src/components/checkout/delivery-date-modal.tsx
+++ src/components/checkout/delivery-date-modal.tsx
@@ -30,7 +30,7 @@ export function DeliveryDateModal({
         i === 0
           ? "Mañana"
           : date
-              .toLocaleDateString("es-AR", { weekday: "long" })
+              .toLocaleDateString("es-AR", { weekday: "short" })
               .replace(/^./, (c) => c.toUpperCase());
       return { formattedDate, label };
     });
@@ -57,7 +57,7 @@ export function DeliveryDateModal({
         }`}
       />
       <div
-        className={`relative w-full max-w-sm rounded-lg bg-background-primary-default p-6 space-y-6 transition-transform duration-300 transform ${
+        className={`relative w-full max-w-sm bg-background-primary-default p-6 pt-5 pb-10 border-t border-t-border-primary-default space-y-6 transition-transform duration-300 transform ${
           open ? "scale-100 opacity-100" : "scale-95 opacity-0"
         }`}
       >
@@ -66,46 +66,50 @@ export function DeliveryDateModal({
             <XMarkIcon className="h-6 w-6" />
           </Button>
         </div>
-        <div className="space-y-4">
-          <h2 className="body-01-semibold">Fecha de envío</h2>
-          <div className="grid grid-cols-3 gap-4">
-            {" "}
-            {dates.map((d, i) => (
-              <button
-                key={i}
-                onClick={() => setSelectedDate(i)}
-                className={`body-02-semibold flex flex-col items-start space-y-1 rounded-md border p-4 text-left ${
-                  selectedDate === i
-                    ? "ring-2 ring-border-primary-default"
-                    : "border-border-primary-default"
-                }`}
-              >
-                <p className="body-01-semibold">{d.formattedDate}</p>
-                <p className="body-02-regular text-text-secondary-default capitalize">
-                  {d.label}
-                </p>
-              </button>
-            ))}
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <h2 className="body-01-regular">Fecha de envío</h2>
+            <div className="inline-grid grid-cols-3 gap-4">
+              {" "}
+              {dates.map((d, i) => (
+                <button
+                  key={i}
+                  onClick={() => setSelectedDate(i)}
+                  className={`body-02-semibold flex flex-col items-center justify-between space-y-1 border w-[80px] h-[88px] p-2 text-left ${
+                    selectedDate === i
+                      ? "ring-2 ring-border-primary-default"
+                      : "border-border-primary-default"
+                  }`}
+                >
+                  <p className="body-01-semibold">{d.formattedDate}</p>
+                  <p className="body-02-regular text-text-secondary-default capitalize">
+                    {d.label}
+                  </p>
+                </button>
+              ))}
+            </div>
           </div>
-          <h2 className="body-01-semibold">Rango horario de envío</h2>
-          <RadioGroup
-            value={timeRange}
-            onValueChange={setTimeRange}
-            className="gap-4"
-          >
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="10hs a 13hs" id="time-morning" />
-              <label htmlFor="time-morning" className="body-02-regular">
-                10hs a 13hs
-              </label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="17hs a 20hs" id="time-evening" />
-              <label htmlFor="time-evening" className="body-02-regular">
-                17hs a 20hs
-              </label>
-            </div>
-          </RadioGroup>
+          <div className="space-y-3">
+            <h2 className="body-01-regular">Rango horario de envío</h2>
+            <RadioGroup
+              value={timeRange}
+              onValueChange={setTimeRange}
+              className="gap-4"
+            >
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="10hs a 13hs" id="time-morning" />
+                <label htmlFor="time-morning" className="body-02-regular">
+                  10hs a 13hs
+                </label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="17hs a 20hs" id="time-evening" />
+                <label htmlFor="time-evening" className="body-02-regular">
+                  17hs a 20hs
+                </label>
+              </div>
+            </RadioGroup>
+          </div>
         </div>
         <Button onClick={handleConfirm} size="lg" className="w-full">
           Confirmar
