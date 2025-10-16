5b01ee7d5a03e0db52ce615254e53cd000dacb33
Juanjo <calvoledesma91@gmail.com>
2025-10-16 11:24:01 -0300

    top-banner changed background color and text for spetial mother's day

--- src/components/common/top-banner.tsx
+++ src/components/common/top-banner.tsx
@@ -12,9 +12,9 @@ export function TopBanner() {
       initial={{ y: -50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
-      className="fixed top-0 inset-x-0 z-50 bg-background-surface-primary-invert text-text-primary-invert body-02-regular text-center py-2"
+      className="fixed top-0 inset-x-0 z-50 bg-background-fill-danger-default text-text-primary-invert body-02-bold text-center py-2"
     >
-      25% OFF en Efectivo o Transferencia
+      10% OFF EXTRA por el Día de la Madre
     </motion.div>
   );
 }
